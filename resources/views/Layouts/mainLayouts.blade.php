5672849d03b0ba0c332f728847c476169dd594df
Nuhu wambali <100964150+NuhuWambali@users.noreply.github.com>
2023-08-29 09:19:55 +0300

    integrating layouts

--- resources/views/Layouts/mainLayouts.blade.php
+++ resources/views/Layouts/mainLayouts.blade.php
@@ -2,21 +2,7 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Employee Leave</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="{{asset('user/vendors/mdi/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="{{asset('user/vendors/css/vendor.bundle.base.css')}}">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <!-- End Plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="{{asset('user/css/style.css')}}">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="{{asset('user/images/favicon.png')}}" />
+    @include(_header)
   </head>
   <body>
     <div class="container-scroller">
